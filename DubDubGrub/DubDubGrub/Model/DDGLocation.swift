3180e33f83fe81854ab1bf8d0d586e8bbbd44792
wiz-boc <wiz_kidz@outlook.com>
2023-07-02 12:02:38 -0500

    Create DDGLocation model

---
+++ DubDubGrub/DubDubGrub/Model/DDGLocation.swift
@@ -0,0 +1,46 @@
+//
+//  DDGLocation.swift
+//  DubDubGrub
+//
+//  Created by wizz on 7/2/23.
+//
+
+import CloudKit
+
+
+struct DDGLocation {
+    
+    static let kName = "name"
+    static let kDescription = "description"
+    static let kSquareAsset = "squareAsset"
+    static let kBannerAsset = "bannerAsset"
+    static let kAddress = "address"
+    static let kLocation = "location"
+    static let kWebsiteURL = "websiteURL"
+    static let KPhoneNumber = "phoneNumber"
+    
+    
+    let ckRecordID: CKRecord.ID
+    let name: String
+    let description: String
+    let squareAsset: CKAsset!
+    let bannerAsset: CKAsset!
+    let address: String
+    let location: CLLocation
+    let websiteURL: String
+    let phoneNumber: String
+    
+    init(record: CKRecord){
+        ckRecordID = record.recordID
+        name = record[DDGLocation.kName] as? String ?? "N/A"
+        description = record[DDGLocation.kDescription] as? String ?? "N/A"
+        squareAsset = record[DDGLocation.kSquareAsset] as? CKAsset
+        bannerAsset = record[DDGLocation.kBannerAsset] as? CKAsset
+        address = record[DDGLocation.kAddress] as? String ?? "N/A"
+        location = record[DDGLocation.kLocation] as? CLLocation ?? CLLocation(latitude: 0, longitude: 0)
+        websiteURL = record[DDGLocation.kWebsiteURL] as? String ?? "N/A"
+        phoneNumber = record[DDGLocation.KPhoneNumber] as? String ?? "N/A"
+    }
+}
+
+
