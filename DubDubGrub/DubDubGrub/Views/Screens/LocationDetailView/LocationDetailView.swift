8d15ad53aa92a73eaa4f1c48db7fd4208f8ee241
wiz-boc <wiz_kidz@outlook.com>
2023-07-15 10:14:14 -0500

    implement checkedIn and checkedOut functionality

--- DubDubGrub/DubDubGrub/Views/Screens/LocationDetailView/LocationDetailView.swift
+++ DubDubGrub/DubDubGrub/Views/Screens/LocationDetailView/LocationDetailView.swift
@@ -47,7 +47,7 @@ struct LocationDetailView: View {
                         }
                         
                         Button{
-                            
+                            viewModel.updateCheckInStatus(to: .checkedOut)
                         } label: {
                             LocationActionButton(color: .brandPrimary, imageName: "person.fill.checkmark")
                         }
--- DubDubGrub/DubDubGrub/Views/Screens/LocationDetailView/LocationDetailViewModel.swift
+++ DubDubGrub/DubDubGrub/Views/Screens/LocationDetailView/LocationDetailViewModel.swift
@@ -7,7 +7,9 @@
 
 import SwiftUI
 import MapKit
+import CloudKit
 
+enum CheckInStatus { case checkedIn, checkedOut }
 
 final class LocationDetailViewModel: ObservableObject {
     
@@ -34,5 +36,37 @@ final class LocationDetailViewModel: ObservableObject {
             return
         }
         UIApplication.shared.open(url)
+    }
+    
+    func updateCheckInStatus(to checkInStatus: CheckInStatus){
+        //Retrieve the DDGProfile
+        guard let profileRecordID = CloudKitManager.shared.profileRecordID  else { return }
+        CloudKitManager.shared.fetchRecord(with: profileRecordID) { [weak self] result in
+            guard let self = self else {return}
+            switch result {
+                case .success(let record):
+                    //Create a reference to the location
+                    switch checkInStatus {
+                        case .checkedIn:
+                            record[DDGProfile.KIsCheckedIn] = CKRecord.Reference(recordID: location.id, action: .none)
+                        case .checkedOut:
+                            record[DDGProfile.KIsCheckedIn] = nil
+                    }
+                    //Save the updated profile to CloudKit
+                    CloudKitManager.shared.save(record: record) { result in
+                        switch result {
+                            case .success(_):
+                                //update our checkedinProfiles array
+                                print("Checked in/out Successfully")
+                            case .failure(let failure):
+                                print("❌ Error saving recording ")
+                        }
+                    }
+                case .failure(let failure):
+                    print("❌ Error fetching recording ")
+            }
+        }
+        
+        
     }
 }
